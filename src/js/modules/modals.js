a15b74cb6fbc025da4b490784e4545213aeb2abf
Dmitriy <101661452+DmitriyAvgustinovich@users.noreply.github.com>
2023-04-08 16:26:14 +0500

    Update modals.js

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,54 +1,52 @@
 const modals = () => {
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
 
-        const triggers = document.querySelectorAll(triggerSelector)
-        const modal = document.querySelector(modalSelector)
-        const close = document.querySelector(closeSelector)
+    const bindModal = (triggerSelector, modalSelector, closeSelector) => {
+        const triggers = document.querySelectorAll(triggerSelector);
+        const modal = document.querySelector(modalSelector);
+        const close = document.querySelector(closeSelector);
 
-        function closeModal() {
-            modal.style.display = 'none'
-            document.body.classList.remove('modal-open')
-        }
+        const closeModal = () => {
+            modal.style.display = 'none';
+            document.body.classList.remove('modal-open');
+        };
 
         document.addEventListener('keydown', (e) => {
             if (e.code === 'Escape') {
-                closeModal()
+                closeModal();
             }
-        })
+        });
 
         triggers.forEach(btn => {
             btn.addEventListener('click', (e) => {
-                if (e.target) {
-                    e.preventDefault()
-                }
+                e.preventDefault();
 
-                modal.style.display = 'block'
-                document.body.classList.add('modal-open')
-            })
-        })
+                modal.style.display = 'block';
+                document.body.classList.add('modal-open');
+            });
+        });
 
         close.addEventListener('click', () => {
-            closeModal()
-        })
+            closeModal();
+        });
 
         modal.addEventListener('click', (e) => {
             if (e.target === modal) {
-                closeModal()
+                closeModal();
             }
-        })
-    }
+        });
+    };
 
-    function showModalByTime(selector, time) {
+    const showModalByTime = (selector, time) => {
         setTimeout(() => {
-            document.querySelector(selector).style.display = 'block'
-            document.body.classList.add('modal-open')
-        }, time)
-    }
+            document.querySelector(selector).style.display = 'block';
+            document.body.classList.add('modal-open');
+        }, time);
+    };
 
-    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close')
-    bindModal('.phone_link', '.popup', '.popup .popup_close')
-    showModalByTime('.popup', 5000)
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+    showModalByTime('.popup', 5000);
 
-}
+};
 
-export default modals
+export default modals;
